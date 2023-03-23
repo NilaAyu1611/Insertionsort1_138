7a2135792208011db8bc3c77b6f1920ac1ff7c13
NilaAyu1611 <nilaayucw@gmail.com>
2023-03-23 16:01:41 +0700

    membuat kondisi da menampilkan pesan

--- Insertionsort1/Insertionsort1.cpp
+++ Insertionsort1/Insertionsort1.cpp
@@ -11,4 +11,15 @@ void input()
         cout << "Masukkan Jumlah Data pada Array : ";   // Mmebuaut inputan jumlah elemnet Array
         cin >> n;                                       // Memanggil variabel inputan n
 
+        if (n <= 20)
+        {   // Mmebuat kondisi n tidak lebih dari 20
+            break;
+
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";    // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+
 

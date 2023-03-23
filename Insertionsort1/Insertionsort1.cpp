acb15ab022a6af1a0bdb3b172f5c9141babeaa1d
NilaAyu1611 <nilaayucw@gmail.com>
2023-03-23 16:25:46 +0700

    simpan nilai temp

--- Insertionsort1/Insertionsort1.cpp
+++ Insertionsort1/Insertionsort1.cpp
@@ -51,7 +51,9 @@ void insertionsort()
         {                                   // arr[j] lebih besar daripada temp
             arr[j + 1] = arr[j];            // a. simpan arr[j] dalam variabel arr [j + 1]
             j--;                            // b. Decrement nilai j by 1
+
         }
 
+        arr[j + 1] = temp;              // 5. Simpan nilai temp ke dalam arr[j + 1]
 
     }
